fe6565d484680ecae04ea792c288c1a83d92d1fb
papapalo-vida <pvidaich@hotmail.com>
2026-09-18 19:41:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service worker minimo: rende l'app installabile e mette in cache solo la "scocca".
+// I dati (adesioni) arrivano sempre dalla rete, mai dalla cache.
+const CACHE = 'bowling-v1';
+const SHELL = ['./', 'index.html', 'manifest.json', 'icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return; // API Google: sempre rete
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
